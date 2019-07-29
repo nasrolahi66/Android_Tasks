be3d94935ee6aed7ef9ceb3de835cde008d395e2
Nasrolahi <snasrolahi93@gmail.com>
2019-07-29 16:24:52 +0330

    Session3

--- app/src/main/java/com/example/android_tasks/MainActivity.java
+++ app/src/main/java/com/example/android_tasks/MainActivity.java
@@ -2,12 +2,57 @@ package com.example.android_tasks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+//String tag="MyTag";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MYTAG","onCreate event");
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MYTAG","onStart event");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MYTAG","onResume event");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MYTAG","onPause event");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MYTAG","onStop event");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MYTAG","onDestroy event");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+         Log.d("MYTAG","onRestart event");
+
+
+    }
+
 }
---
+++ app/src/main/java/com/example/android_tasks/Session3_task2_Activity.java
@@ -0,0 +1,13 @@
+package com.example.android_tasks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Session3_task2_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_session3_task2_);
+    }
+}
